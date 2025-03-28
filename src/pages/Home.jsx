c525bb74e4e4a15ce41937311c82486befe5831e
moizuddin404 <shaikhmoizuddin786@gmail.com>
2025-03-28 10:43:49 +0530

    Modified Home Page to Contain All Knowledge Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,40 @@
-const Home = () => {
+import React, { useState } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import "../css/Home.css";
+import { useEffect } from "react";
+import AllKnowledgeCards from "../components/AllKC";
 
-}
+const Home = () => {
+  const [kcData, setKcData] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    // console.log(token);
+    axios.get("http://localhost:8000/knowledge-card/", {
+      params: {token: token}
+    })
+    .then(response => {
+      console.log("Response: ",response.data);
+      console.log("Response: ",kcData);
+      setKcData(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching knowledge card:', error);
+    })
+    .finally(()=> console.log("Completed"));
+  
+  }, []);
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="kc-container">
+        <div className="all-cards-section">
+            <AllKnowledgeCards cardData={kcData} />
+        </div>
+        </div>
+    </div>
+  );
+};
 
 export default Home;
